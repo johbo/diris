a905cc1e067a1f8c06729f3fa82b20958fda7727
Johannes Bornhold <johannes@bornhold.name>
2014-06-09 17:17:14 +0200

    Init new repo, Die Iris

---
+++ TODO.rst
@@ -0,0 +1,34 @@
+
+==============
+ Talk to iris
+==============
+
+Needed:
+
+- [X] empty project with dub
+
+  Used `dub`
+
+- [X] add msgpack as dependency
+
+  Straight forward with `dub`.
+
+- [X] msgpack a structure
+
+- [ ] msgpack an iris structure
+
+- [X] add zeromq as dependency
+
+  easy thanks to `dub`
+
+- [ ] ZeroMQ - open tcp endpoint
+
+- [ ] send exampe structure iris.ping // echo.echo
+
+- [ ] receive result
+
+- [ ] create structure out of the result
+
+- [ ] Zookeeper: discover a service
+
+  At first, can put the tcp endpoint directly
---
+++ source/app.d
@@ -0,0 +1,95 @@
+import std.exception;
+import std.string;
+import std.stdio;
+
+import deimos.zmq.zmq;
+import msgpack;
+
+
+void main(string[] args)
+{
+  args = args[1 .. $];
+  enforce(args.length == 1, "Need zmq endpoint as first argument");
+
+  auto endpoint = args[0];
+  auto my_endpoint = "todo";
+
+  writefln("Connecting to %s", endpoint);
+
+  // TODO: should be wrapped in struct to handle it like a resource?
+  // or a class?
+  void* ctx = zmq_ctx_new();
+  scope(exit) { zmq_ctx_destroy(ctx); }
+
+  // Create a socket
+  // TODO: same as above, struct or class ?
+  void* req = zmq_socket(ctx, ZMQ_REQ);
+  scope(exit) { zmq_close(req); }
+
+  // Connect it
+  zmq_connect(req, toStringz(endpoint));
+
+  // Prepare message and send it
+  // TODO: That looks ugly, moving bits and bytes around, should be somehow
+  // wrapped in a nice way
+  zmq_msg_t request;
+  zmq_msg_init_size(&request, 5);
+
+  ///memcpy (zmq_msg_data (&request), "Hello", 5);
+  ///Slicing calls memcpy internally.
+  immutable(void*) source = "Hello".ptr;
+
+  (zmq_msg_data(&request))[0..5] = source[0..5];
+
+  writeln("Client: Sending Hello");
+  zmq_sendmsg(req, &request, 0);
+  zmq_msg_close(&request);
+
+  /* Iris message format
+
+     0: ID, a uuid
+     1: type, one of REQ, REP, ACK, NACK, ERROR
+     2: subject, string
+     3: headers, msgpack dict / map
+     4. body, msgpack encoded body
+
+  */
+
+  // Looks like a tuple, pack headers and pack body
+  // So this should be good enough to simulate it
+
+  auto example_frame = [
+    "uuid",
+    "msgtype",
+    "subject",
+    "packed_headers",
+    "packed_body"
+  ];
+
+
+
+}
+
+
+struct Message {
+
+  enum ACK = "ACK",
+    REP = "REP",
+    REQ = "REQ",
+    NACK = "NACK",
+    ERROR = "ERROR";
+}
+
+unittest {
+  assert(Message.ACK == "ACK");
+}
+
+
+unittest {
+  auto p = Packer();
+  p.packArray("Hello", "World", "!");
+
+  immutable ubyte[] expected_data = [
+    147, 165, 72, 101, 108, 108, 111, 165, 87, 111, 114, 108, 100, 161, 33];
+  assert(p.stream.data == expected_data);
+}
